afc15fabe5ff4c23cab1877e2ad680a591ec9ac5
Jose_Julian_CA <jose.julian.celio.arellano@gmail.com>
2023-01-30 01:40:01 -0600

    setting nav

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -7,13 +7,5 @@ router.get('/employees', (req, res) => {
     res.send("gray");
 });
 
-/*router.get('/sing', (req, res) => {
-    try {
-        res.render('../view/sing-in/sing-in');
-    } catch (error) {
-        res.json({message: "Page not found", error: error});
-    }
-});
-*/
 
 export default router;
